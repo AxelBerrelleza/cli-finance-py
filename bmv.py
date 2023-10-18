73b38649b94c1ef968ccaadde8e55ef71077e0a5
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2023-10-18 13:59:12 -0600

    upgrade bmv command, new subcommands

--- bmv.py
+++ bmv.py
@@ -18,14 +18,16 @@ watchlist: list = [
 ]
 
 endpoints: dict = {
-    'prices': 'https://api.databursatil.com/v1/precios?token=' + API_TOKEN + '&bolsa=BMV&emisora_serie='
+    'prices': 'https://api.databursatil.com/v1/precios?token=' + API_TOKEN + '&bolsa=BMV&emisora_serie=',
+    'dividend': 'https://api.databursatil.com/v1/dividendos?token=' + API_TOKEN + '&emisora_serie=',
+    'credits': 'https://api.databursatil.com/v1/creditos?token=' + API_TOKEN,
 }
 
-@click.group()
+@click.group(help='To request in the Mexican stock exchange')
 def cli():
     pass
 
-@click.command(help='Request current prices of your watchlist')
+@cli.command(help='Request current prices of your watchlist')
 def wl():
     url = endpoints["prices"] + watchlist[1]
 
@@ -34,7 +36,21 @@ def wl():
 
     pprint.pprint(response.json())
 
-cli.add_command(wl)
+@cli.command(help='gets the most recent dividend')
+@click.argument('symbol', type=click.STRING)
+def div(symbol):
+    url = endpoints['dividend'] + symbol
+
+    response = requests.get(url)
+    response.raise_for_status()
+    pprint.pprint(response.json())
+
+@cli.command("api-credits")
+def api_credits():
+    url = endpoints['credits']
+    response = requests.get(url)
+    response.raise_for_status()
+    pprint.pprint(response.json())
 
 if __name__ == '__main__':
     cli()
