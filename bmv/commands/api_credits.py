4e7061c37f30df026aba5f238aa5b8d979c6463a
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2023-11-04 10:06:22 -0600

    upgrate api-credits command, output with rich package

--- bmv/commands/api_credits.py
+++ bmv/commands/api_credits.py
@@ -1,11 +1,11 @@
-import click
 import requests
-import pprint
 from app import cli, endpoints
+from rich import print as rprint
 
 @cli.command("api-credits")
 def api_credits():
     url = endpoints['credits']
     response = requests.get(url)
     response.raise_for_status()
-    pprint.pprint(response.json())
+    
+    rprint('[bold]Remaining: %s' % (response.json()['Disponibles']))
